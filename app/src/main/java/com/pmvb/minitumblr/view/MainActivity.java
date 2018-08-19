cdb7120c90e8f2d072fb0f01c8ec2b1f8a9721e4
Martín Vásquez <pedromvasquezb@gmail.com>
2018-08-19 18:02:40 -0500

    Modify MainActivity

--- app/src/main/java/com/pmvb/minitumblr/view/MainActivity.java
+++ app/src/main/java/com/pmvb/minitumblr/view/MainActivity.java
@@ -1,13 +1,20 @@
-package com.pmvb.minitumblr;
+package com.pmvb.minitumblr.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.pmvb.minitumblr.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView postList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        postList = findViewById(R.id.post_list);
     }
 }
